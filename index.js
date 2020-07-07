1801d078a07ec7496fa4739c48d20abe3edad76c
Arkadiusz Pasek <thevik444@gmail.com>
2020-07-07 14:43:59 +0200

    Rss Parser!

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const { default: Axios } = require('axios');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+
+app.get('/rss', async (req, res) => {
+  const url = req.query.url;
+  if (url) {
+    const response = await axios.get(url);
+    res.type('text/plain');
+    res.send(response.data);
+  }
+
+})
+
+app.listen(PORT, () => {
+  console.log('App listening on port: ' + PORT);
+})
