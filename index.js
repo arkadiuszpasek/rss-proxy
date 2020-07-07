8d71e52956b2978e4a07fec62fee5e19095edd24
Arkadiusz Pasek <thevik444@gmail.com>
2020-07-07 14:54:50 +0200

    process port

--- index.js
+++ index.js
@@ -9,11 +9,16 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 app.get('/rss', async (req, res) => {
-  const url = req.query.url;
-  if (url) {
-    const response = await axios.get(url);
-    res.type('text/plain');
-    res.send(response.data);
+  console.log('Got request', req);
+  try {
+    const url = req.query.url;
+    if (url) {
+      const response = await axios.get(url);
+      res.type('text/plain');
+      res.send(response.data);
+    }
+  } catch (err) {
+    console.log(err);
   }
 
 })
